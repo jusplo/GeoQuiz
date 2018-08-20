33accee06e8d3ba12ed373354d366a7e61467513
jusplo <ploonka@gmail.com>
2018-08-20 17:55:48 +0200

    commit3

--- app/src/main/java/com/example/justyna/geoquiz/QuizActivity.java
+++ app/src/main/java/com/example/justyna/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package com.example.justyna.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
@@ -10,10 +11,13 @@ import android.widget.Toast;
 
 
 
+
 public class QuizActivity extends AppCompatActivity {
+    private static final String TAG = "QuizActivity";
     private Button mTrueButton;
     private Button mFalseButton;
     private Button mNextButton;
+    private Button mPreviousButton;
     private TextView mQuestionTextView;
 
     private Question[] mQuestionBank = new Question[] {
@@ -31,9 +35,17 @@ public class QuizActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "Wywołanie metody: onCreate(Bundle)");
         setContentView(R.layout.activity_quiz);
 
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+        /*mQuestionTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });*/
 
         mTrueButton = (Button) findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
@@ -66,7 +78,48 @@ public class QuizActivity extends AppCompatActivity {
             }
         });
         updateQuestion();
+
+
+        mPreviousButton = (Button) findViewById(R.id.previous_button);
+        mPreviousButton.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 mCurrentIndex = (mCurrentIndex -1) % mQuestionBank.length;
+                 updateQuestion();
+                 }
+        });
+        updateQuestion();
     }
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "Wywołanie metody: onStart()");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "Wywołanie metody: onResume()");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "Wywołanie metody: onPause()");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "Wywołanie metody: onStop()");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "Wywołanie metody: onDestroy()");
+    }
+
     private void updateQuestion() {
         int question = mQuestionBank[mCurrentIndex].getTextResId();
         mQuestionTextView.setText(question);
@@ -83,4 +136,5 @@ public class QuizActivity extends AppCompatActivity {
         Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
     }
 }
+
 
